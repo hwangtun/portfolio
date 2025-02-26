5f473511f6c4c877b5e4ebd2ba01bccc272c7fb6
hwangtun <tanhoanglevo3110@gmail.com>
2025-02-26 14:07:00 +0700

    commit

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'hwangtun',
+  devUsername: "Võ Lê Hoàng Tân",
+  github: 'https://github.com/hwangtun',
+  facebook: 'https://web.facebook.com/hoangtan.vole/',
+  linkedIn: 'https://www.linkedin.com/in/v%C3%B5-l%C3%AA-ho%C3%A0ng-t%C3%A2n-472918332/',
+  twitter: 'https://x.com/hwangtun_vle31',
+  stackOverflow: 'https://stackoverflow.com/users/22880453/v%c3%b5-l%c3%aa-ho%c3%a0ng-t%c3%a2n',
+  leetcode: "https://leetcode.com/said3812/",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  skills: ['React', 'HTML', 'JavaScript', 'CSS', 'JavaSwing', 'Python', 'C#', 'MySQL', 'SQLSever', 'AWS', 'Figma'],
+  timezone: '+6'
+};
