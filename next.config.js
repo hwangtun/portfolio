44932f14dc947735a6d152f9f05a26cd786b08d4
hwangtun <tanhoanglevo3110@gmail.com>
2025-02-26 14:12:46 +0700

    commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
-const path = require('path')
- 
+const path = require("path");
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
+    domains: [
+      "media.dev.to",
+      "avatars.githubusercontent.com",
+      "github-readme-stats.vercel.app",
+      "github-readme-streak-stats.herokuapp.com",
+      "stardev.io",
+      "github-profile-summary-cards.vercel.app",
+      "github-profile-trophy.vercel.app",
+    ],
     dangerouslyAllowSVG: true,
-  }
-}
+    unoptimized: true,
+  },
+  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "/portfolio/" : "",
+  output: "export",
+  trailingSlash: true, 
+  reactStrictMode: true,
+};
